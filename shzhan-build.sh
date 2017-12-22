e1ef2f856cb920c49a2ac2779160e6dd2993d5d7
LinTeX9527 <LinTeX9527@yeah.net>
2017-12-22 11:14:46 +0800

    升级cmake到3.9.6版本，成功编译当前工程

---
+++ shzhan-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+rm -rf build.paho
+mkdir build.paho
+cd build.paho
+echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
+cmake ..
+make
+
+# no need to proceed.
+#python ../test/mqttsas2.py localhost 1883 1885 &
+#ctest -VV --timeout 600
+#kill %1
+#killall mosquitto
